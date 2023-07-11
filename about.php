56ed7358b41664d1c96893c9bc89d690eb6b6249
OnlineDynamic <68251154+OnlineDynamic@users.noreply.github.com>
2023-07-11 18:32:25 +0100

    Update about.php

--- about.php
+++ about.php
@@ -1,15 +1,15 @@
   <div style="margin:0 auto;"> <br />
     <fieldset style="padding: 10px; border: 2px solid #000;">
-      <legend>Template Plugin Info</legend>
+      <legend>DynamicPixels Plugin Info</legend>
       <div style="overflow: hidden; padding: 10px;">
     <div>
       <div id='credits'>
-        <b>Template Plugin Developed By:</b><br />
+        <b>Plugin Developed By:</b><br />
 		<br />
-        John Doe (jdoe)<br />
+        Stuart Ledingham of DynamicPixels<br />
 		<br />
-        <a href='https://github.com/FalconChristmas/fpp-plugin-Template'>Git Repository</a><br>
-        <a href='https://github.com/FalconChristmas/fpp-plugin-Template/issues'>Bug Reporter</a><br>
+        <a href='https://github.com/OnlineDynamic/DynamicPixelsFPPPlugin'>Git Repository</a><br>
+        <a href='https://github.com/OnlineDynamic/DynamicPixelsFPPPlugin/issues'>Bug Reporter</a><br>
 		<br />
       </div>
     </div>
