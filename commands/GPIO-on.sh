8a1ca08040f2f1f853b39d08833f6d64b70c5351
OnlineDynamic <68251154+OnlineDynamic@users.noreply.github.com>
2023-07-13 23:35:12 +0100

    Update GPIO-on.sh

--- commands/GPIO-on.sh
+++ commands/GPIO-on.sh
@@ -3,9 +3,20 @@
 # GPIO-On.sh - Turn on one of the Pi's GPIO Outputs       #
 ###########################################################
 
+if [ $# -lt 1 ];
+then
+echo "# GPIO on Script"
+echo "# Usage:"
+echo "# GPIO-On.sh GPIO#"
+echo "# example:"
+echo "# GPIO-On.sh 76"
+exit 0
+fi
+
 # Set the GPIO number (NOT the pin number)
-#GPIO="64"
-GPIO="76"
+#GPIO="64" (pi zero)
+#GPIO="76" (Pi4)
+GPIO=$1
 
 # Make sure the pin is setup for output.
 /opt/fpp/src/fpp -G ${GPIO},Output
