08563a2923db1605f3d1fdde2f22533884185241
OnlineDynamic <68251154+OnlineDynamic@users.noreply.github.com>
2023-07-13 23:35:55 +0100

    Update GPIO-off.sh

--- commands/GPIO-off.sh
+++ commands/GPIO-off.sh
@@ -3,9 +3,20 @@
 # GPIO-Off.sh - Turn on one of the Pi's GPIO Outputs       #
 ###########################################################
 
+if [ $# -lt 1 ];
+then
+echo "# GPIO off Script"
+echo "# Usage:"
+echo "# GPIO-Off.sh GPIO#"
+echo "# example:"
+echo "# GPIO-Off.sh 76"
+exit 0
+fi
+
 # Set the GPIO number (NOT the pin number)
-#GPIO="64"
-GPIO="76"
+#GPIO="64" (pi zero)
+#GPIO="76" (Pi4)
+GPIO=$1
 
 # Make sure the pin is setup for output.
 /opt/fpp/src/fpp -G ${GPIO},Output
